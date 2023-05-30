9168be4515c5ac07e5a1c727ee15676385a7478c
akrem-jr <133533756+akrem-jr@users.noreply.github.com>
2023-05-30 15:48:25 +0300

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,15 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        #li declare empty list the called li #
+        li=[]
+        for i in range(1,n+1):
+                    
+            if i%3==0 and i%5==0:
+                li.append("FizzBuzz")
+            elif i%3==0:
+                    li.append("Fizz")
+            elif i%5==0:
+                    li.append("Buzz")
+            else:
+                li.append(str(i))
+        return li
